f788d9db126cd10dd5cd107e085c327bdc467c5b
WalerE-Creator <github@e.walesfamily.co.nz>
2025-04-16 11:03:51 +1200

    Add files via upload

---
+++ pyhouse.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.left(45)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(45)
+turtle.forward(110)
+turtle.right(90)
+turtle.forward(141)
+turtle.right(90)
+turtle.forward(110)
+turtle.right(90)
+turtle.forward(141)
