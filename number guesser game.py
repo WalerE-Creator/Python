d7f8dbaafc79a947603e03ad916254d5bfae733d
WalerE-Creator <github@e.walesfamily.co.nz>
2025-04-17 10:46:26 +1200

    Update number guesser game.py
    added difficulty system

--- number guesser game.py
+++ number guesser game.py
@@ -1,8 +1,12 @@
 import random
 
-hiddennum = random.randint(1,11)
 
+difficulty = input('''Please select your difficulty,
+            1) Easy 1-10
+            2) Medium 1-20
+            3) Hard 1-30''')
 
+highnum = int(difficulty) * 10
 
 while True:
     printrules = input("Do you want to read the rules? (y/n)")
@@ -12,18 +16,17 @@ while True:
     else:
         print("Nope - gotta be y or n!. Try again...")
 
-        
-
-
 if printrules == 'y':
 
-    print('''   The rules are:
+    print(f'''   The rules are:
 
-        You have 10 guesses to guess a random number between 1 & 10,
+        You have 10 guesses to guess a random number between 1 & {highnum} you choose,
         you want to guess the number correctly in as few guesses as possible.
         ''')
 
-guessnum = input("Please guess a number between 1 and 10.")
+guessnum = input(f'Please guess a number between 1 and {highnum}.')
+
+hiddennum = random.randint(1, highnum)
 
 guesses = 1
 
@@ -47,8 +50,5 @@ while True:
 
         print("You got it wrong, please try again")
 
-        guessnum = input("Please guess another number between 1 and 10.")
+        guessnum = input(f'Please guess another number between 1 and {highnum}.')
 #to make a hint system
-
-
-    
