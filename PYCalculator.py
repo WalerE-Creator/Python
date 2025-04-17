cb23e936f4385c41c8444321aa90d274d87c4e72
walesenz <github@e.walesfamily.co.nz>
2025-04-17 13:15:33 +1200

    Update PYCalculator.py

--- PYCalculator.py
+++ PYCalculator.py
@@ -1,48 +1,37 @@
 print("Welcome to the PYCalculator!")
 
 num1 = input("Please enter your 1st number")
-
 num2 = input("Please enter your 2nd number")
-
 op = input("Please enter an operator, if you don't know the correct symbol, type ref.")
 
 while True:
-
-    if op == '*':
-
-        print(f'The answer is: {int(num1) * int(num2)}')
-
+    if op == "*":
+        print(f"The answer is: {int(num1) * int(num2)}")
         break
 
-    if op == '+':
-
-        print(f'The answer is: {int(num1) + int(num2)}')
-
+    if op == "+":
+        print(f"The answer is: {int(num1) + int(num2)}")
         break
 
-    if op == '-':
-
-        print(f'The answer is: {int(num1) - int(num2)}')
-
+    if op == "-":
+        print(f"The answer is: {int(num1) - int(num2)}")
         break
 
-    if op == '/':
-
-        print(f'The answer is: {int(num1) / int(num2)}')
-
+    if op == "/":
+        print(f"The answer is: {int(num1) / int(num2)}")
         break
 
-    if op == 'ref':
+    if op == "ref":
 
-        print('''
+        print(
+            """
                 Type * for times
                 Type / for divide by
                 Type + for addition
                 Type - for subtraction
-                ''')
+                """
+        )
 
-        op = input("Please enter an operator, if you don't know the correct symbol, type ref.")
-
-
-
-    
+        op = input(
+            "Please enter an operator, if you don't know the correct symbol, type ref."
+        )
